7a8fae1a3f01435290e711e519395284e1f66529
amitCohen2 <amitcohen435@gmail.com>
2023-04-20 20:39:42 +0300

    Add CourtBallGameApp Entity
    1. Create CourtBallGame app with the starting files
    2. Add CourtBallGame app model to the player.models
    3. Add CourtBallGame app to the meet_balls/settings.py
    4. Add Tests

---
+++ court_ball_game/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import CourtBallGame
+
+admin.site.register(CourtBallGame)
---
+++ court_ball_game/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from court.models import Court
+from player.models import BallGame
+
+
+class CourtBallGame(models.Model):
+    court = models.ForeignKey(Court, on_delete=models.CASCADE)
+    ball_game = models.CharField(
+        max_length=100,
+        choices=BallGame.choices
+    )
+
+    class Meta:
+        unique_together = ('court', 'ball_game')
