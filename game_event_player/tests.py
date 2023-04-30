685f21e44d057c577ef657fa8276e67dfd30e7c3
MaayanMashhadi <maayanmas4@gmail.com>
2023-04-30 19:10:50 +0300

    Modify game_event_player fields name
    modify the fields: game_event_id to game_event
    and player_id to player
    Also, creating new migration file 00002 and change tests.py

--- game_event_player/tests.py
+++ game_event_player/tests.py
@@ -7,7 +7,7 @@ from game_event.models import GameEvent
 from player.models import Player, BallGame
 from court.models import Court
 from decimal import Decimal
-from datetime import datetime
+from django.utils import timezone
 
 
 ID1 = 2
@@ -82,7 +82,7 @@ def saved_court():
 
 @pytest.fixture
 def saved_game_event(saved_court):
-    game_event = GameEvent.objects.create(id=ID1, time=datetime.today(), level_of_game=3,
+    game_event = GameEvent.objects.create(id=ID1, time=timezone.now(), level_of_game=3,
                                           min_number_of_players=2, max_number_of_players=5, court=saved_court,
                                           ball_game='Basketball')
     return game_event
@@ -90,7 +90,7 @@ def saved_game_event(saved_court):
 
 @pytest.fixture
 def saved_game_event2(saved_court):
-    game_event = GameEvent.objects.create(id=ID2, time=datetime.today(), level_of_game=3,
+    game_event = GameEvent.objects.create(id=ID2, time=timezone.now(), level_of_game=3,
                                           min_number_of_players=2, max_number_of_players=5, court=saved_court,
                                           ball_game='Basketball')
     return game_event
@@ -98,7 +98,7 @@ def saved_game_event2(saved_court):
 
 @pytest.fixture
 def saved_game_event_player(saved_game_event, saved_player):
-    return GameEventPlayer.objects.create(game_event_id=saved_game_event, player_id=saved_player,
+    return GameEventPlayer.objects.create(game_event=saved_game_event, player=saved_player,
                                           ball_responsible=False)
 
 
@@ -106,31 +106,31 @@ def saved_game_event_player(saved_game_event, saved_player):
 class TestGameEventPlayerModel:
 
     def test_game_event_player_creation(self, saved_game_event, saved_player):
-        game_event_player = GameEventPlayer.objects.create(game_event_id=saved_game_event, player_id=saved_player,)
+        game_event_player = GameEventPlayer.objects.create(game_event=saved_game_event, player=saved_player,)
         game_event_player.full_clean()
-        assert game_event_player.game_event_id == saved_game_event
-        assert game_event_player.player_id == saved_player
+        assert game_event_player.game_event == saved_game_event
+        assert game_event_player.player == saved_player
         assert game_event_player.ball_responsible is False
 
     def test_filter_players_by_event(self, saved_game_event, saved_game_event2, players):
-        GameEventPlayer.objects.create(game_event_id=saved_game_event, player_id=players[0], ball_responsible=False)
-        GameEventPlayer.objects.create(game_event_id=saved_game_event, player_id=players[1], ball_responsible=False)
-        GameEventPlayer.objects.create(game_event_id=saved_game_event, player_id=players[2], ball_responsible=False)
-        GameEventPlayer.objects.create(game_event_id=saved_game_event2, player_id=players[0], ball_responsible=False)
-        GameEventPlayer.objects.create(game_event_id=saved_game_event2, player_id=players[1], ball_responsible=False)
-        p1 = GameEventPlayer.objects.filter(player_id=players[0])
-        events = sorted([entry.game_event_id.id for entry in p1])
+        GameEventPlayer.objects.create(game_event=saved_game_event, player=players[0], ball_responsible=False)
+        GameEventPlayer.objects.create(game_event=saved_game_event, player=players[1], ball_responsible=False)
+        GameEventPlayer.objects.create(game_event=saved_game_event, player=players[2], ball_responsible=False)
+        GameEventPlayer.objects.create(game_event=saved_game_event2, player=players[0], ball_responsible=False)
+        GameEventPlayer.objects.create(game_event=saved_game_event2, player=players[1], ball_responsible=False)
+        p1 = GameEventPlayer.objects.filter(player=players[0])
+        events = sorted([entry.game_event.id for entry in p1])
         expected_events = sorted([ID1, ID2])
         assert expected_events == events
-        e1 = GameEventPlayer.objects.filter(game_event_id=saved_game_event)
-        players = sorted([entry.player_id.user for entry in e1], key=str)
+        e1 = GameEventPlayer.objects.filter(game_event=saved_game_event)
+        players = sorted([entry.player.user for entry in e1], key=str)
         expected_players = sorted(players, key=str)
         assert expected_players == players
 
     def test_unique_together_constraint(self, saved_game_event, saved_player):
-        GameEventPlayer.objects.create(game_event_id=saved_game_event, player_id=saved_player, ball_responsible=False)
+        GameEventPlayer.objects.create(game_event=saved_game_event, player=saved_player, ball_responsible=False)
         with pytest.raises(IntegrityError):
-            GameEventPlayer.objects.create(game_event_id=saved_game_event, player_id=saved_player,
+            GameEventPlayer.objects.create(game_event=saved_game_event, player=saved_player,
                                            ball_responsible=False)
 
     def test_get_game_event_player(self, saved_game_event_player):
@@ -139,20 +139,20 @@ class TestGameEventPlayerModel:
 
     def test_create_game_event_player_with_empty_player(self, saved_game_event):
         with pytest.raises(IntegrityError):
-            GameEventPlayer.objects.create(game_event_id=saved_game_event, player_id=Player.objects.create(),
+            GameEventPlayer.objects.create(game_event=saved_game_event, player=Player.objects.create(),
                                            ball_responsible=False)
 
     def test_create_game_event_player_with_empty_game_event(self, saved_player):
         with pytest.raises(IntegrityError):
-            GameEventPlayer.objects.create(game_event_id=GameEvent.objects.create(), player_id=saved_player,
+            GameEventPlayer.objects.create(game_event=GameEvent.objects.create(), player=saved_player,
                                            ball_responsible=False)
 
     def test_create_game_event_player_with_blank_ball_responsible(self, saved_game_event, saved_player):
         with pytest.raises(ValidationError):
-            GameEventPlayer.objects.create(game_event_id=saved_game_event, player_id=saved_player,
+            GameEventPlayer.objects.create(game_event=saved_game_event, player=saved_player,
                                            ball_responsible='').full_clean()
 
     def test_create_game_event_player_with_invalid_ball_responsible(self, saved_game_event, saved_player):
         with pytest.raises(ValidationError):
-            GameEventPlayer.objects.create(game_event_id=saved_game_event, player_id=saved_player,
+            GameEventPlayer.objects.create(game_event=saved_game_event, player=saved_player,
                                            ball_responsible='invalid - should be boolean').full_clean()
