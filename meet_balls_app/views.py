38913ae4a492109634ab209db73ab5a8a1213422
Ohad Segal <segalohad@gmail.com>
2023-05-09 15:00:36 +0300

    Add login-page and static players users
    1.Add login, logout pathes to the meetballs/urls.py file
    2.Add login, logut methods to render relvant pages in the meet_balls_app/views.py file
    3.Add login.html template file
    4.Modify base.html file with the relvant new href's
    5.Add tests for login and logout tests for login template
    6.Add static players users

--- meet_balls_app/views.py
+++ meet_balls_app/views.py
@@ -1,5 +1,29 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, logout, login
+from django.contrib import messages
 
 
 def home(request):
     return render(request, 'meet_balls_app/home.html')
+
+
+def loginUser(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return render(request, 'meet_balls_app/home.html',
+                          {'user': {'is_authenticated': True, 'username': username}})
+        else:
+            error_message = "Invalid username or password."
+            messages.error(request, error_message)
+            return render(request, 'meet_balls_app/login.html', {'user': {'is_authenticated': False, 'username': None}})
+    else:
+        return render(request, 'meet_balls_app/login.html', {'user': {'is_authenticated': False, 'username': None}})
+
+
+def logoutUser(request):
+    logout(request)
+    return render(request, 'meet_balls_app/home.html', {'user': {'is_authenticated': False}})
